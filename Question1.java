cbeccb382f9f03d20e4214afd96bffcb141913fb
Ultron-sk <sabariks2063@gmail.com>
2023-07-26 21:50:22 +0530

    Question 1 commited

---
+++ Question1.java
@@ -0,0 +1,20 @@
+ interface Shape{
+    double area=0.0;
+    double perimeter=0.0;
+   
+    void input();
+   void calculate();
+   void display();
+}
+
+
+
+public class Question1{
+
+public static void main(String[] args) {
+    
+}
+
+
+
+}
