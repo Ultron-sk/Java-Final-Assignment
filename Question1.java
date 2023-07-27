3f9fd489e0194f4aa01d8553b775c90de843ce92
Ultron-sk <sabariks2063@gmail.com>
2023-07-27 17:04:49 +0530

    commit

--- Question1.java
+++ Question1.java
@@ -7,13 +7,34 @@ interface Shape{
    void display();
 }
 
+class Rectangle implements Shape{
+
+   @Override
+   public void input() {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'input'");
+   }
+
+   @Override
+   public void calculate() {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'calculate'");
+   }
+
+   @Override
+   public void display() {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'display'");
+   }
+
+}
 
 
 public class Question1{ 
     public static void main(String[] args) 
     {
 
-      
+
     }
 
 
