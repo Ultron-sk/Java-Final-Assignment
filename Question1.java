21fd70ad394cc5a196727c7325458b213b61736c
Ultron-sk <sabariks2063@gmail.com>
2023-07-27 17:08:11 +0530

    commit

--- Question1.java
+++ Question1.java
@@ -29,6 +29,28 @@ class Rectangle implements Shape{
 
 }
 
+class Circle implements Shape{
+
+   @Override
+   public void input() {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'input'");
+   }
+
+   @Override
+   public void calculate() {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'calculate'");
+   }
+
+   @Override
+   public void display() {
+      // TODO Auto-generated method stub
+      throw new UnsupportedOperationException("Unimplemented method 'display'");
+   }
+
+}
+
 
 public class Question1{ 
     public static void main(String[] args) 
