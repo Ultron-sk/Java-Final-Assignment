5fd78cfea4f31b8e45fbe9c8c61cb0ebd55b62f4
Ultron-sk <sabariks2063@gmail.com>
2023-08-19 23:21:13 +0530

    Question2 Completed

--- Question2_Constructor.java
+++ Question2_Constructor.java
@@ -1,13 +1,27 @@
 class CricketPlayer{
-
+   CricketPlayer(){
+    System.out.println("I am a Cricket Player");
+   }
 }
 class King extends CricketPlayer{
-    
-}
-
-
-public  class Question2_Constructor{
-    public static void main(String[] args) {
-        
+    King(){
+        System.out.println("I am Virat Kohli");
     }
 }
+
+@SuppressWarnings("unused")
+public  class Question2_Constructor{
+    public static void main(String[] args) {
+        CricketPlayer cricketPlayer = new King();
+    }
+}
+/*
+ * Constructor Key Points:
+ * 
+ * Constructor is a memeber method which gets invoked when the object is created
+ * Name of the constructor is same as the class and return nothing (no return type)
+ * If no constructor is defined JVM will create a default constructor
+ * The first line of every constructor is the super() method , even though the class not extending anything as Object class is parent of all class
+ * Whenever we create the object of subclass it calls the super class constructor because of super()
+ * 
+ */
