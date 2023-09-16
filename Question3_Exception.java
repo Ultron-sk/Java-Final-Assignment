3df537206aaa69b978fbbf928365ce3108df3ea5
Ultron-sk <sabariks2063@gmail.com>
2023-09-16 22:51:07 +0530

    Question 3 finished

---
+++ Question3_Exception.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Question3_Exception {
+    public static void main(String[] args) {
+        try{
+            perform();
+        }catch(ArithmeticException ae){
+            System.out.println(ae.getMessage());
+        }catch(Exception e){
+            System.out.println("Some Other Exception");
+        }
+
+        System.out.println("Program Ended");
+         
+    }
+
+   public static void perform() throws ArithmeticException{
+      Scanner sc = new Scanner(System.in);
+         System.out.println("Enter a  number::");
+         int x = sc.nextInt();
+         if(x<0){
+ 
+            throw new ArithmeticException("Negative  Number is Given As Input");
+         }
+         sc.close();
+   }
+
+}
+
+
