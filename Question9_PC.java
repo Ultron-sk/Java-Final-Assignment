c31569be661991e1fe271f079662cd42fbc1f800
Ultron-sk <sabariks2063@gmail.com>
2023-10-13 00:29:56 +0530

    Question  9 Started

---
+++ Question9_PC.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Question9_PC {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Producer implements Runnable{
+  
+  List<Integer> queue = new ArrayList<>();
+
+    @Override
+    public void run() {
+
+    }
+}
+
+class Consumer implements Runnable
+{
+ 
+
+
+    @Override
+    public void run() {
+
+    }
+    
+}
+
